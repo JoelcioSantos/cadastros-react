df4ae507a501f1527b06d2291e70dc32a7ef39c5
Joelcio <joelciodossantosjunior@gmail.com>
2022-08-14 22:16:49 -0300

    Criando base do dashboard com box responsivo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,28 @@
-import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Dashboard } from '../pages';
 import { useAppThemeContext, useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
 
     /*const { toggleTheme } = useAppThemeContext();*/
-    const { toggleDrawerOpen } = useDrawerContext();
+    const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+
+    useEffect(() => {
+        setDrawerOptions([
+            {
+                label: 'Página inicial',
+                icon: 'home',
+                path: '/pagina-inicial',
+            }
+            
+        ]);
+    }, []);
 
     return (
         <Routes>
             {/* <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}> Toggle theme </Button>} /> */}
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="secondary" onClick={toggleDrawerOpen}> Toggle Drawer </Button>} />
+            <Route path="/pagina-inicial" element={<Dashboard/>} />
             <Route path="*" element={<Navigate to="/pagina-inicial" />}/>
         </Routes>
     );
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,16 +1,45 @@
 import { Avatar, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, useTheme, Icon, useMediaQuery } from '@mui/material';
 import { Box } from '@mui/system';
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 import {useDrawerContext} from '../../contexts';
 
 interface IAppThemeProviderProps {
     children: React.ReactNode
 }
 
+interface IListItemLinkProps {
+    label: string;
+    icon: string;
+    to: string;
+    onClick: (() => void) | undefined;
+}
+const ListItemLink: React.FC<IListItemLinkProps> = ({to, icon, label, onClick}) => {
+
+    const navigate = useNavigate();
+
+    const resolvedPath = useResolvedPath(to);
+    const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+    const handleClick = () => {
+        navigate(to);
+        onClick?.();
+    };
+
+    return (
+        <ListItemButton selected={!!match} onClick={handleClick}>
+            <ListItemIcon>
+                <Icon>{icon}</Icon>
+            </ListItemIcon>
+            <ListItemText primary = {label} />
+        </ListItemButton>
+    );
+};
+
 export const MenuLateral: React.FC<IAppThemeProviderProps> = ( {children} ) => {
     const theme = useTheme();
     const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
-    const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+    const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
 
     return (
         <>
@@ -24,12 +53,15 @@ export const MenuLateral: React.FC<IAppThemeProviderProps> = ( {children} ) => {
 
                     <Box flex={1}>
                         <List component="nav">
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <Icon>home</Icon>
-                                </ListItemIcon>
-                                <ListItemText primary="Página Inicial" />
-                            </ListItemButton>
+                            {drawerOptions.map(drawerOption => (
+                                <ListItemLink
+                                    key={drawerOption.path}
+                                    icon = {drawerOption.icon}
+                                    to = {drawerOption.path}
+                                    label = {drawerOption.label}
+                                    onClick={smDown ? toggleDrawerOpen : undefined}
+                                />
+                            ))}
                         </List>
                     </Box>
                 </Box>
