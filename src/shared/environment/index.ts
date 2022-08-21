94dbc10f4f5570d6e5969dd08f11e36e25ba7599
Joelcio <joelciodossantosjunior@gmail.com>
2022-08-21 16:00:04 -0300

    Configurando axios e variaveis de ambiente

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,21 @@
-export {};
+export const Environment = {
+    /**
+     * Define a quantidade de linhas a ser carrega por padrão nas listagens
+     */
+    LIMITE_DE_LINHAS: 10,
+
+    /**
+     * Placeholder/hint dos campos de busca
+     */
+    INPUT_DE_BUSCA: 'Pesquisar...',
+
+    /**
+     * Texto retornado quando não há retorno de nenhuma record em uma listagem
+     */
+    LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
+
+    /**
+     * Url base de consulta ao backend
+     */
+    URL_BASE: 'http://localhost:3333',
+};
